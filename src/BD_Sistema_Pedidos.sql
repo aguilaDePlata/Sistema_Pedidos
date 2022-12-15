c842c4a507e630e5a612bb340e11e67936576288
aguilaDeOro <jorgeluis_illescas@hotmail.com>
2022-12-15 07:18:11 -0500

    feature: script db

---
+++ src/BD_Sistema_Pedidos.sql
@@ -0,0 +1,147 @@
+USE master
+CREATE DATABASE C3_BD_PEDIDOS
+GO
+
+USE C3_BD_PEDIDOS
+GO
+
+CREATE TABLE [Cargo] (
+  [ID_Cargo] int IDENTITY(1,1) NOT NULL,
+  [Cargo] nvarchar(50) NULL,
+  [Sueldo] decimal(10,2) NULL,
+  CONSTRAINT [_copy_2] PRIMARY KEY CLUSTERED ([ID_Cargo])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Cliente] (
+  [ID_Cliente] int IDENTITY(1,1) NOT NULL,
+  [Nombre] nvarchar(50) NULL,
+  [Apellidos] nvarchar(50) NULL,
+  [Tipo_Cliente] nvarchar(20) NOT NULL,
+  [Nro_Documento] nvarchar(11) NOT NULL,
+  [Direccion] nvarchar(255) NULL,
+  [Distrito] nvarchar(50) NULL,
+  [Telefono] nvarchar(20) NULL,
+  [Activo] bit NULL,
+  CONSTRAINT [_copy_5] PRIMARY KEY CLUSTERED ([ID_Cliente])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Comprobante] (
+  [ID_Comprobante] int IDENTITY(1,1) NOT NULL,
+  [ID_Pedido] int NULL,
+  [Tipo_Comprobante] nvarchar(20) NULL,
+  [Fecha_Emision] date NULL,
+  [Subtotal_Comp] decimal(10,2) NULL,
+  [Descuento] decimal(10,2) NULL,
+  [Igv] decimal(10,2) NULL,
+  [Valor_Total] decimal(10,2) NULL,
+  PRIMARY KEY CLUSTERED ([ID_Comprobante])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Detalle_Pedido] (
+  [ID_Pedido] int NULL,
+  [ID_Producto] int NULL,
+  [Cantidad] int NULL,
+  [Precio_Venta] decimal(10,2) NULL,
+  [Subtotal_Prod] decimal(10,2) NULL
+)
+GO
+
+CREATE TABLE [Empleado] (
+  [ID_Empleado] int IDENTITY(1,1) NOT NULL,
+  [Nombres] nvarchar(50) NULL,
+  [Apellidos] nvarchar(50) NULL,
+  [DNI] int NULL,
+  [Direccion] nvarchar(255) NULL,
+  [Distrito] nvarchar(50) NULL,
+  [Edad] int NULL,
+  [Telefono] nvarchar(20) NULL,
+  [ID_Cargo] int NULL,
+  [Fecha_Contrato] date NULL,
+  [Activo] bit NULL,
+  CONSTRAINT [_copy_1] PRIMARY KEY CLUSTERED ([ID_Empleado])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Marca] (
+  [ID_Marca] int IDENTITY(1,1) NOT NULL,
+  [Marca] nvarchar(50) NULL,
+  PRIMARY KEY CLUSTERED ([ID_Marca])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Modelo] (
+  [ID_Modelo] int IDENTITY(1,1) NOT NULL,
+  [Modelo] nvarchar(50) NULL,
+  PRIMARY KEY CLUSTERED ([ID_Modelo])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Pedido] (
+  [ID_Pedido] int IDENTITY(1,1) NOT NULL,
+  [ID_Cliente] int NULL,
+  [ID_Empleado] int NULL,
+  [Fecha_Pedido] date NULL,
+  [Fecha_MaxEntrega] date NULL,
+  [Valor_Total] decimal(10,2) NULL,
+  [Activo] bit NULL,
+  CONSTRAINT [_copy_3] PRIMARY KEY CLUSTERED ([ID_Pedido])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Producto] (
+  [ID_Producto] int IDENTITY(1,1) NOT NULL,
+  [Nombre_Producto] nvarchar(50) NULL,
+  [Descripcion] nvarchar(255) NULL,
+  [Precio_Venta] decimal(10,2) NULL,
+  [ID_Marca] int NULL,
+  [ID_Modelo] int NULL,
+  [ID_Proveedor] int NULL,
+  [Stock] int NULL,
+  [Activo] bit NULL,
+  CONSTRAINT [_copy_4] PRIMARY KEY CLUSTERED ([ID_Producto])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+CREATE TABLE [Proveedor] (
+  [ID_Proveedor] int IDENTITY(1,1) NOT NULL,
+  [Nombres_Prov] nvarchar(50) NULL,
+  [Apellidos_Prov] nvarchar(50) NULL,
+  [Direccion] nvarchar(255) NULL,
+  [Distrito] nvarchar(50) NULL,
+  [Telefono] nvarchar(20) NULL,
+  [Activo] bit NULL,
+  PRIMARY KEY CLUSTERED ([ID_Proveedor])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+GO
+
+ALTER TABLE [Comprobante] ADD CONSTRAINT [FK_Comprobante_Pedido] FOREIGN KEY ([ID_Pedido]) REFERENCES [Pedido] ([ID_Pedido])
+GO
+ALTER TABLE [Detalle_Pedido] ADD CONSTRAINT [FK_DetalleP_Pedido] FOREIGN KEY ([ID_Pedido]) REFERENCES [Pedido] ([ID_Pedido])
+GO
+ALTER TABLE [Detalle_Pedido] ADD CONSTRAINT [FK_DetalleP_Producto] FOREIGN KEY ([ID_Producto]) REFERENCES [Producto] ([ID_Producto])
+GO
+ALTER TABLE [Empleado] ADD CONSTRAINT [FK_Empleado_Cargo] FOREIGN KEY ([ID_Cargo]) REFERENCES [Cargo] ([ID_Cargo])
+GO
+ALTER TABLE [Pedido] ADD CONSTRAINT [FK_Pedido_Cliente] FOREIGN KEY ([ID_Cliente]) REFERENCES [Cliente] ([ID_Cliente])
+GO
+ALTER TABLE [Pedido] ADD CONSTRAINT [FK_Pedido_Empleado] FOREIGN KEY ([ID_Empleado]) REFERENCES [Empleado] ([ID_Empleado])
+GO
+ALTER TABLE [Producto] ADD CONSTRAINT [FK_Producto_Marca] FOREIGN KEY ([ID_Marca]) REFERENCES [Marca] ([ID_Marca])
+GO
+ALTER TABLE [Producto] ADD CONSTRAINT [FK_Producto_Modelo] FOREIGN KEY ([ID_Modelo]) REFERENCES [Modelo] ([ID_Modelo])
+GO
+ALTER TABLE [Producto] ADD CONSTRAINT [FK_Producto_Proveedor] FOREIGN KEY ([ID_Proveedor]) REFERENCES [Proveedor] ([ID_Proveedor])
+GO
+
---
+++ src/BD_Sistema_Pedidos_Inserts.sql
@@ -0,0 +1,78 @@
+﻿USE C3_BD_PEDIDOS
+GO
+
+SELECT @@LANGUAGE
+SET LANGUAGE SPANISH --Para poder ingresar fecha en formato DD-MM-YYY sin errores
+GO
+
+INSERT INTO Cargo VALUES('Vendedor', 1400)
+INSERT INTO Cargo VALUES('Despachador', 1000)
+GO
+
+INSERT INTO Cliente VALUES('Pedro','Salas Rojas','Natural','49672364','Av. Peru 485','SMP','954781364',1)
+INSERT INTO Cliente VALUES('Margarita','Perez Rivas','Natural','59910012','Av. Los Heroes 164','SJM','980994671',1)
+INSERT INTO Cliente VALUES('Paula','Ciro Bustamante','Natural','54189623','Av. Los Girasoles 098','Surco','964949121',1)
+INSERT INTO Cliente VALUES('Jaime','Jimenez Valencia','Natural','10012015','Jr. Libertarios 465','San Borja','956005489',1)
+GO
+
+INSERT INTO Empleado VALUES('Marjhorie','Aguirre Abanto','59012347','Av. Centenario 408','SJM',27,'962121348',1,'13/07/2020',1)
+INSERT INTO Empleado VALUES('Daniel','Condori Martinez','79006451','Av. Los Heroes 840','SJM',22,'905236741',2,'25/02/2022',1)
+GO
+
+INSERT INTO Marca VALUES('Generico')
+INSERT INTO Marca VALUES('El Chino')
+INSERT INTO Marca VALUES('Mattel')
+INSERT INTO Marca VALUES('Pokemon')
+GO
+
+INSERT INTO Modelo VALUES('Oso')
+INSERT INTO Modelo VALUES('Leon')
+INSERT INTO Modelo VALUES('Stitch')
+INSERT INTO Modelo VALUES('Pikachu')
+GO
+
+INSERT INTO Proveedor VALUES('Alico','Jimenez Salas','Jr. Mesa Redonda 964','Lima','984561347',1)
+INSERT INTO Proveedor VALUES('Rosario','Silva Salgado','Jr. Mesa Redonda 905','Lima','941693123',1)
+GO
+
+INSERT INTO Producto VALUES('Leon Mediano','Leon de peluche mediano de color naranja y con camiseta roja',
+							12.50,2,2,1,36,1)
+INSERT INTO Producto VALUES('Leon Pequeño','Leon de peluche chico de color naranja y con camiseta celeste',
+							9.00,2,2,1,20,1)
+INSERT INTO Producto VALUES('Stitch Futbolista','Peluche de Stitch con camiseta de futbol de Brasil, Argentina o Peru',
+							15.00,3,3,2,47,1)
+INSERT INTO Producto VALUES('Pikachu Surfista','Peluche de Pikachu macho o hembra con tabla de surf',
+							22.00,4,4,2,20,1)
+GO
+
+INSERT INTO Pedido VALUES(1,1,'15/05/2022','15/05/2022',45.00,1)
+INSERT INTO Pedido VALUES(2,1,'08/06/2022','13/06/2022',21.50,1)
+INSERT INTO Pedido VALUES(3,1,'19/08/2022','23/08/2022',18.00,1)
+INSERT INTO Pedido VALUES(4,1,'12/06/2022','15/06/2022',44.00,1)
+GO
+
+INSERT INTO Detalle_Pedido VALUES(1,3,3,15.00,45.00)
+INSERT INTO Detalle_Pedido VALUES(2,1,1,12.50,12.50)
+INSERT INTO Detalle_Pedido VALUES(2,2,1,9.00,9.00)
+INSERT INTO Detalle_Pedido VALUES(3,2,2,9.00,18.00)
+INSERT INTO Detalle_Pedido VALUES(4,4,2,22.00,44.00)
+GO
+
+INSERT INTO Comprobante VALUES(1,'Boleta','15/05/2022',45.00,0,0,45.00)
+INSERT INTO Comprobante VALUES(2,'Boleta','13/06/2022',21.50,0,0,21.50)
+INSERT INTO Comprobante VALUES(3,'Boleta','23/08/2022',18.00,0,0,18.00)
+INSERT INTO Comprobante VALUES(4,'Boleta','15/06/2022',44.00,0,0,44.00)
+GO
+
+--Comprobacion de Datos insertados
+SELECT * FROM Cargo
+SELECT * FROM Cliente
+SELECT * FROM Empleado
+SELECT * FROM Marca
+SELECT * FROM Modelo
+SELECT * FROM Proveedor
+SELECT * FROM Producto
+SELECT * FROM Pedido
+SELECT * FROM Detalle_Pedido
+SELECT * FROM Comprobante
+GO
