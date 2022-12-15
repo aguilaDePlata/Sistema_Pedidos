b330116d9d6a5c03a2dac0e1366fa016727890d7
aguilaDeOro <jorgeluis_illescas@hotmail.com>
2022-12-15 12:09:50 -0500

    upd pedido

--- src/Sistema_Pedidos/Vista/FormPedido.cs
+++ src/Sistema_Pedidos/Vista/FormPedido.cs
@@ -12,7 +12,36 @@ namespace Sistema_Pedidos
 {
     public partial class frmPedido : Form
     {
+        private PedidoDto nuevoPedido;
 
+        public frmPedido()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPedido_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscarCliente_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscarProducto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+        } 
     }
 }
-    
